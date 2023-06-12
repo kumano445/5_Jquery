7388dfc87e1769fb51fe0e55bcbc49779d86b4b7
yoshiko <ttkmysk021@gmail.com>
2023-06-12 12:54:31 +0900

    trying

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,40 +1,3 @@
-// 変数settingsに設定情報などを格納
-// const settings = {
-  // 実行するURL。実行するURLのことをエンドポイントと言います。
-  // url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
-  // サーバーに送るメソッド。GETは情報を取得したい場合のメソッド。
-  // "method": "GET",
-  // }
-
-  // Ajaxの実行
-  //.doneが通信成功した時の処理、”response”が引数となっていて通信した結果を受け取っている
-  // messageエラー発生時にエラーメッセージが表示されるフィールド。
-  // $(".search-btn").on("click", function(){
-    // $("#search-input"). val();
-  // })
-  // $(".message")
-  // $.ajax(settings).done(function (response) {
-
-      // メモ：タイトルtitle	 作者(著作)dc:creator	出版社dc:publisher　書籍情報link
-  // メモ：itemsレスポンスに含まれる図書・雑誌へのパーマリンクを列挙。最大でopensearch:itemsPerPageの数まで繰り返す。
-
-
-  //.failが通信に失敗した時の処理、”err”にサーバーから送られてきたエラー内容を受け取っている。
-  // }).fail(function (err) {
-    //メモ：三項演算子 条件式 ? trueの処理 : falseの処理　エラー内容を表示。
-  // });
-
-  // $(function() {
-    // $(".reset-btn")
-  // })
-
-//考え方メモ
-//① 検索ワードにキーワードを入力。
-//②検索ボタンを押す。
-// ③検索結果にタイトル、作者、出版社の書籍情報が出る。
-// ④リセットを押すと全て消える。
-//⑤ 「？」などの検索が失敗すると「検索結果が見つかりませんでした。別のキーワードで検索して下さい。」が表示される。
-
 $(function() {
   $(".search-btn").on("click", function(){
     var searchWord = $("#search-input").val();
@@ -96,3 +59,41 @@ $(function() {
     $("#search-input").val(""); // 検索ワードをクリア
   });
 });
+
+// 変数settingsに設定情報などを格納
+// const settings = {
+  // 実行するURL。実行するURLのことをエンドポイントと言います。
+  // url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
+  // サーバーに送るメソッド。GETは情報を取得したい場合のメソッド。
+  // "method": "GET",
+  // }
+
+  // Ajaxの実行
+  //.doneが通信成功した時の処理、”response”が引数となっていて通信した結果を受け取っている
+  // messageエラー発生時にエラーメッセージが表示されるフィールド。
+  // $(".search-btn").on("click", function(){
+    // $("#search-input"). val();
+  // })
+  // $(".message")
+  // $.ajax(settings).done(function (response) {
+
+      // メモ：タイトルtitle	 作者(著作)dc:creator	出版社dc:publisher　書籍情報link
+  // メモ：itemsレスポンスに含まれる図書・雑誌へのパーマリンクを列挙。最大でopensearch:itemsPerPageの数まで繰り返す。
+
+
+  //.failが通信に失敗した時の処理、”err”にサーバーから送られてきたエラー内容を受け取っている。
+  // }).fail(function (err) {
+    //メモ：三項演算子 条件式 ? trueの処理 : falseの処理　エラー内容を表示。
+  // });
+
+  // $(function() {
+    // $(".reset-btn")
+  // })
+
+//考え方メモ
+//① 検索ワードにキーワードを入力。
+//②検索ボタンを押す。
+// ③検索結果にタイトル、作者、出版社の書籍情報が出る。
+// ④リセットを押すと全て消える。
+//⑤ 「？」などの検索が失敗すると「検索結果が見つかりませんでした。別のキーワードで検索して下さい。」が表示される。
+
