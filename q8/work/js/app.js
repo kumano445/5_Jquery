b029cc28f29522802ea18307a2cf02f530042068
yoshiko <ttkmysk021@gmail.com>
2023-07-05 20:41:25 +0900

    six

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -3,13 +3,13 @@ $(function() {
     $(".message").remove();
     $(".lists").empty();
 
-    if (response && response["@graph"] && response["@graph"][0]["items"]) {
-      const books = response["@graph"][0]["items"];
+    if (response && response["@graph"] && response["@graph"][0].items) {
+      const books = response["@graph"][0].items;
       $.each(books, function(index, book) {
         const title = book.title ? book.title : "不明";
         const author = book["dc:creator"] ? book["dc:creator"] : "不明";
         const publisher = book["dc:publisher"] ? book["dc:publisher"][0] : "不明";
-        const listItemHTML = '<li class="lists-item"><div class="list-inner"><p>タイトル：' + (title ? title : "不明") + '</p><p>作者：' + (author ? author : "不明") + '</p><p>出版社：' + (publisher ? publisher : "不明") + '</p><a href="' + (book.link["@id"] + '" target="_blank">書籍情報</a></div></li>');
+        const listItemHTML = `<li class="lists-item"><div class="list-inner"><p>タイトル：${title}</p><p>作者：${author}</p><p>出版社：${publisher}</p><a href="${book.link["@id"]}" target="_blank">書籍情報</a></div></li>`;
         $(".lists").prepend(listItemHTML);
       });
     } else {
@@ -17,11 +17,11 @@ $(function() {
     }
   }
 
-  function handleSearchFailure(jqXHR, textStatus, errorThrown)  {
+  function handleSearchFailure(jqXHR) {
     $(".lists").empty();
     $(".message").remove();
     if (jqXHR.status === 0) {
-      $(".lists").before('<div class="message">正常に通信できませんでした。インターネットの接続の確認をしてください。</div>');
+      $(".lists").before('<div class="message">正常に通信できませんでした。インターネットの接続を確認してください。</div>');
     } else {
       $(".lists").before('<div class="message">ネットワークエラーが発生しました。再度試してください。</div>');
     }
@@ -29,26 +29,26 @@ $(function() {
 
   $(".search-btn").on("click", function() {
     const searchWord = $("#search-input").val();
-    let pageCount = 1; 
+    let pageCount = 1;
 
     if (searchWord) {
       $(".lists").empty();
-      pageCount++; 
+      pageCount++;
       $.ajax({
         url: "https://ci.nii.ac.jp/books/opensearch/search",
         method: "GET",
         data: {
           title: searchWord,
           format: "json",
-          p: 1,
+          p: pageCount,
           count: 20
         }
       })
       .done(function(response) {
         handleSearchSuccess(response);
       })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        handleSearchFailure(jqXHR, textStatus, errorThrown);
+      .fail(function(jqXHR) {
+        handleSearchFailure(jqXHR);
       });
     } else {
       $(".lists").empty();
