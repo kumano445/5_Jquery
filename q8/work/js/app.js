1f2a002895a468ef5e5d03134c7ae2d48314d915
yoshiko <ttkmysk021@gmail.com>
2023-07-10 20:42:13 +0900

    seven

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,20 +1,31 @@
 $(function() {
   let searchLog = ""; // ひとつ前の検索ワードを保持する変数
   let pageCount = 1; // ページカウントの初期値を設定
+  let searchResults = []; // 検索結果を保持する配列
 
   function handleSearchSuccess(response) {
     $(".message").remove();
-    $(".lists").empty();
 
     if (response && response["@graph"] && response["@graph"][0].items) {
       const books = response["@graph"][0].items;
+
+    if (pageCount === 1) {
+      $(".lists").empty();
+      searchResults = []; // 検索結果をリセット
+    }
+
       $.each(books, function(index, book) {
         const title = book.title ? book.title : "不明";
         const author = book["dc:creator"] ? book["dc:creator"] : "作者不明";
         const publisher = book["dc:publisher"] ? book["dc:publisher"][0] : "不明";
         const listItemHTML = `<li class="lists-item"><div class="list-inner"><p>タイトル：${title}</p><p>作者：${author}</p><p>出版社：${publisher}</p><a href="${book.link["@id"]}" target="_blank">書籍情報</a></div></li>`;
         $(".lists").prepend(listItemHTML);
+        searchResults.push(book); // 検索結果を配列に追加
       });
+    if (books.length === 20) {
+      const loadMoreButton = '<button class="load-more-btn">もっと表示</button>';
+      $(".lists").append(loadMoreButton);
+    }
     } else {
       $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
     }
@@ -27,10 +38,13 @@ $(function() {
     if (jqXHR.status === 0) {
       $(".lists").before('<div class="message">正常に通信できませんでした。インターネットの接続を確認してください。</div>');
     } else if (jqXHR.status === 400) {
+      $(".message").remove();
       $(".lists").before('<div class="message">リクエストされたページが見つかりません。</div>');
     } else if (jqXHR.status === 404) {
+      $(".message").remove();
       $(".lists").before('<div class="message">ページが見つかりません。</div>');
     } else {
+      $(".message").remove();
       $(".lists").before('<div class="message">ネットワークエラーが発生しました。再度試してください。</div>');
     }
   }
@@ -46,6 +60,9 @@ $(function() {
         pageCount = 1;
         searchLog = searchWord;
       }
+      $(document).on("click", ".load-more-btn", function() {
+        pageCount++;
+        const searchWord = $("#search-input").val();
       $.ajax({
         url: "https://ci.nii.ac.jp/books/opensearch/search",
         method: "GET",
@@ -73,5 +90,6 @@ $(function() {
     $(".lists").empty();
     $(".message").remove();
     $("#search-input").val("");
+    searchResults = []; // 検索結果をクリア
   });
 });
