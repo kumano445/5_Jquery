c63e30c13c7d6cf9c9c60aa6272c8004814a02b6
yoshiko <ttkmysk021@gmail.com>
2023-07-17 13:32:43 +0900

    seven creat

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,14 +1,15 @@
 $(function() {
   let searchLog = ""; // ひとつ前の検索ワードを保持する変数
   let pageCount = 1; // ページカウントの初期値を設定
+  let previousResults = []; // 過去の検索結果を保持する配列
 
   function handleSearchSuccess(response) {
     $(".message").remove();
     $(".lists").empty();
-
     if (response && response["@graph"] && response["@graph"][0].items) {
       const books = response["@graph"][0].items;
-      const displayedResults = books.slice(0, pageCount * 20); // 表示する結果数を調整
+      previousResults = previousResults.concat(books); // 過去の検索結果に新しい結果を追加
+      const displayedResults = previousResults.slice(0, pageCount * 20); // 表示する結果数を調整
 
       $.each(displayedResults,function(index, book) {
         const title = book.title ? book.title : "不明";
@@ -17,6 +18,10 @@ $(function() {
         const listItemHTML = `<li class="lists-item"><div class="list-inner"><p>タイトル：${title}</p><p>作者：${author}</p><p>出版社：${publisher}</p><a href="${book.link["@id"]}" target="_blank">書籍情報</a></div></li>`;
         $(".lists").prepend(listItemHTML);
       });
+
+      const totalResults = previousResults.length; // 結果の総件数を取得
+      console.log("表示件数:", displayedResults.length, "件"); // 表示された件数をコンソールに出力
+      console.log("総件数:", totalResults, "件"); // 総件数をコンソールに出力
     } else {
       $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
     }
@@ -46,6 +51,7 @@ $(function() {
     if (searchWord !== searchLog) {
       pageCount = 1;
       searchLog = searchWord;
+      previousResults = []; // 新しい検索ワードが入力された場合は前回の検索結果をリセット
     }
 
     $.ajax({
@@ -75,5 +81,6 @@ $(function() {
     $("#search-input").val("");
     searchLog = "";
     pageCount = 1;
+    previousResults = [];
   });
 });
