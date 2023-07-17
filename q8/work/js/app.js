bf29706606c06c09588a29a1c9376835de05d103
yoshiko <ttkmysk021@gmail.com>
2023-07-17 13:17:46 +0900

    seven creat

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,7 +1,6 @@
 $(function() {
   let searchLog = ""; // ひとつ前の検索ワードを保持する変数
   let pageCount = 1; // ページカウントの初期値を設定
-  let previousResults = []; // 過去の検索結果を保持する配列
 
   function handleSearchSuccess(response) {
     $(".message").remove();
@@ -9,9 +8,8 @@ $(function() {
 
     if (response && response["@graph"] && response["@graph"][0].items) {
       const books = response["@graph"][0].items;
-      previousResults = previousResults.concat(books); // 過去の検索結果に新しい結果を追加
-      const displayedResults = previousResults.slice(0, pageCount * 20); // 表示する結果数を調整
-      // displayedResults,
+      const displayedResults = books.slice(0, pageCount * 20); // 表示する結果数を調整
+
       $.each(displayedResults,function(index, book) {
         const title = book.title ? book.title : "不明";
         const author = book["dc:creator"] ? book["dc:creator"] : "作者不明";
@@ -27,52 +25,48 @@ $(function() {
   function handleSearchFailure(jqXHR) {
     $(".lists").empty();
     $(".message").remove();
-    console.log(jqXHR.status);
     if (jqXHR.status === 400) {
       $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
       console.error("HTTP Status:", jqXHR.status);
-    // } else if (jqXHR.status === 404) {
-      // $(".lists").before('<div class="message">ページが見つかりません。</div>');
-    // } 
-    // else {
-      // $(".lists").before('<div class="message">ネットワークエラーが発生しました。再度試してください。</div>');
+    } else {
+      $(".lists").before('<div class="message">予期せぬエラーが発生しました。再度試してください。</div>');
     }
   }
 
   $(".search-btn").on("click", function() {
-    const searchWord = $("#search-input").val();
-    {
-    if (searchWord.trim() !== "") 
+    const searchWord = $("#search-input").val().trim();
+    if (searchWord === "") {
       $(".lists").empty();
-      if (searchWord !== searchLog) {
-        pageCount = 1;
-        previousResults = []; // 新しい検索ワードが入力された場合は前回の検索結果をリセット
-        searchLog = searchWord;
-      }
-      $.ajax({
-        type: "GET",
-        url: "https://ci.nii.ac.jp/books/opensearch/search",
-        data: {
-          title: searchWord,
-          format: "json",
-          p: pageCount,
-          count: 20
-        }
-      })
-        .done(function(response) {
-          handleSearchSuccess(response);
-          pageCount++;
-        })
-        .fail(function(jqXHR) {
-          handleSearchFailure(jqXHR);
-          console.error("Request failed:400");
-        });
-    // } else {
-      // $(".lists").empty();
-      // $(".message").remove();
-      // $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続を確認してください。</div>');
-      // console.error("400");
+      $(".message").remove();
+      $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
+      console.error("HTTP Status: 400");
+      return;
     }
+
+    if (searchWord !== searchLog) {
+      pageCount = 1;
+      searchLog = searchWord;
+    }
+
+    $.ajax({
+      type: "GET",
+      url: "https://ci.nii.ac.jp/books/opensearch/search",
+      data: {
+        title: searchWord,
+        format: "json",
+        p: pageCount,
+        count: 20
+      }
+    })
+      .done(function(response) {
+        handleSearchSuccess(response);
+        pageCount++;
+        console.log("Request succeeded:", pageCount * 20, "items");
+      })
+      .fail(function(jqXHR) {
+        handleSearchFailure(jqXHR);
+        console.error("Request failed:", jqXHR.status);
+      });
   });
 
   $(".reset-btn").on("click", function() {
@@ -81,9 +75,5 @@ $(function() {
     $("#search-input").val("");
     searchLog = "";
     pageCount = 1;
-    previousResults = [];
   });
 });
-
-
-// textStatus, errorThrownいらない
