4a72f32da8285a3dac9335fc12cd2e492f15e7bd
yoshiko <ttkmysk021@gmail.com>
2023-07-25 20:33:56 +0900

    eigth creat

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -3,28 +3,17 @@ $(function() {
   let pageCount = 1; // ページカウントの初期値を設定
   let displayedResults = []; // 表示する結果を保持する配列
 
-  function handleSearchSuccess(response) {
-    $(".message").remove();
-    if (response && response["@graph"] && response["@graph"][0].items) {
-      const books = response["@graph"][0].items;
-      displayedResults = displayedResults.concat(books); // 新しい結果を表示配列に追加
-      displayResults(displayedResults);
-      const totalResults = displayedResults.length; // 結果の総件数を取得
-      console.log("表示件数:", displayedResults.length, "件"); // 表示された件数をコンソールに出力
-      console.log("総件数:", totalResults, "件"); // 総件数をコンソールに出力
-    } else {
-      $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
-    }
-  }
-
   function handleSearchFailure(jqXHR) {
     $(".lists").empty();
     $(".message").remove();
-    if (jqXHR.status === 0) {
-      $(".lists").before('<div class="message">予期せぬエラーが発生しました。再度試してください。</div>');
-    } else {
-      $(".lists").before(`<div class="message">HTTP Status: ${jqXHR.status}</div>`);
+    if (jqXHR.status === 400) {
+      $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
       console.error("HTTP Status:", jqXHR.status);
+    } else if (jqXHR.status === 0) {
+      $(".lists").before('<div class="message">エラー：サーバーに接続できません。</div>');
+      console.error("HTTP Status: 0");
+    } else {
+      $(".lists").before('<div class="message">予期せぬエラーが発生しました。再度試してください。</div>');
     }
   }
 
@@ -41,6 +30,14 @@ $(function() {
 
   $(".search-btn").on("click", function() {
     const searchWord = $("#search-input").val().trim();
+    if (searchWord === "") {
+      $(".lists").empty();
+      $(".message").remove();
+      $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
+      console.error("HTTP Status: 400");
+      return;
+    }
+
     if (searchWord !== searchLog) {
       pageCount = 1;
       searchLog = searchWord;
@@ -58,7 +55,17 @@ $(function() {
       }
     })
       .done(function(response) {
-        handleSearchSuccess(response);
+        $(".message").remove();
+        if (response && response["@graph"] && response["@graph"][0].items) {
+          const books = response["@graph"][0].items;
+          displayedResults = displayedResults.concat(books); // 新しい結果を表示配列に追加
+          displayResults(displayedResults);
+          const totalResults = displayedResults.length; // 結果の総件数を取得
+          console.log("表示件数:", displayedResults.length, "件"); // 表示された件数をコンソールに出力
+          console.log("総件数:", totalResults, "件"); // 総件数をコンソールに出力
+        } else {
+          $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
+        }
         pageCount++;
       })
       .fail(function(jqXHR) {
