df81ff5fd32411c0a076b5a858a3d23d8d645e7b
yoshiko <ttkmysk021@gmail.com>
2023-07-29 10:23:42 +0900

    cret

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,17 +1,16 @@
 $(function() {
   let searchLog = "";
   let pageCount = 1;
-  let displayedResults = [];
+  let displayedResults = []; 
 
   function handleSearchFailure(jqXHR) {
     $(".lists").empty();
     $(".message").remove();
-    if (jqXHR.status === 400) {
+
+    if (jqXHR.status === 0) {
       $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
-      console.error("HTTP Status:", jqXHR.status);
-    } else if (jqXHR.status === 0) {
+    } else if (jqXHR.status === 400) {
       $(".lists").before('<div class="message">エラー：サーバーに接続できません。</div>');
-      console.error("HTTP Status: 0");
     } else {
       $(".lists").before('<div class="message">予期せぬエラーが発生しました。再度試してください。</div>');
     }
@@ -28,20 +27,28 @@ $(function() {
     });
   }
 
+  function success(response) {
+    const data = response["@graph"][0];
+    $(".message").remove();
+    console.log(response);
+    if (data["opensearch:totalResults"] !== 0) {
+      const currentPageItems = data.items.slice(0, pageCount * 20); // 最新の20件を取得
+      displayedResults = displayedResults.concat(currentPageItems); // 新しい結果を表示配列に追加
+      displayResults(displayedResults); // すべての結果を表示
+      console.log("表示件数:", displayedResults.length, "件");
+      console.log("総件数:", data["opensearch:totalResults"], "件");
+      pageCount++;
+    } else {
+      $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
+    }
+  }
+
   $(".search-btn").on("click", function() {
     const searchWord = $("#search-input").val().trim();
-    if (searchWord === "") {
-      $(".lists").empty();
-      $(".message").remove();
-      $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
-      console.error("HTTP Status: 400");
-      return;
-    }
 
     if (searchWord !== searchLog) {
       pageCount = 1;
       searchLog = searchWord;
-      displayedResults = [];
     }
 
     $.ajax({
@@ -54,24 +61,11 @@ $(function() {
         count: 20
       }
     })
-      .done(function(response, textStatus, jqXHR) { // jqXHRオブジェクトを追加
-        $(".message").remove();
-        if (response && response["@graph"] && response["@graph"][0].items) {
-          const books = response["@graph"][0].items;
-          displayedResults = displayedResults.concat(books);
-          displayResults(displayedResults);
-          const totalResults = displayedResults.length;
-          console.log("表示件数:", displayedResults.length, "件");
-          console.log("総件数:", totalResults, "件");
-          console.log("ステータスコード:", jqXHR.status);
-        } else {
-          $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
-        }
-        pageCount++;
+      .done(function(response) {
+        success(response);
       })
       .fail(function(jqXHR) {
         handleSearchFailure(jqXHR);
-        console.error("Request failed:", jqXHR.status);
       });
   });
 
@@ -81,6 +75,5 @@ $(function() {
     $("#search-input").val("");
     searchLog = "";
     pageCount = 1;
-    displayedResults = [];
   });
 });
