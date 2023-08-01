db41f2cf1493a87acc6a5fa2e259d03ded3c5cbd
yoshiko <ttkmysk021@gmail.com>
2023-08-01 12:59:36 +0900

    repair

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,47 +1,47 @@
 $(function() {
   let searchLog = "";
-  let pageCount = 1;
+  let currentPage = 1;
 
   function handleSearchFailure(jqXHR) {
     $(".lists").empty();
     $(".message").remove();
+
     if (jqXHR.status === 0) {
       $(".lists").before('<div class="message">検索ワードを入力してください。</div>');
     } else if (jqXHR.status === 400) {
-      $(".lists").before('<div class="message">サーバーエラーです</div>');
+      $(".lists").before('<div class="message">エラー：サーバーに接続できません。</div>');
     } else {
       $(".lists").before('<div class="message">予期せぬエラーが発生しました。再度試してください。</div>');
     }
   }
 
   function displayResults(results) {
-    $(".lists").empty();
     $.each(results, function(index, book) {
       const title = book.title ? book.title : "不明";
       const author = book["dc:creator"] ? book["dc:creator"] : "作者不明";
       const publisher = book["dc:publisher"] ? book["dc:publisher"][0] : "不明";
       const listItemHTML = `<li class="lists-item"><div class="list-inner"><p>タイトル：${title}</p><p>作者：${author}</p><p>出版社：${publisher}</p><a href="${book.link["@id"]}" target="_blank">書籍情報</a></div></li>`;
-      $(".lists").prepend(listItemHTML);
+      $(".lists").append(listItemHTML);
     });
   }
+
   function success(response) {
     const data = response["@graph"][0];
     $(".message").remove();
     if (data["opensearch:totalResults"] !== 0) {
-      const currentPageItems = data.items.slice(0, pageCount * 20); 
-      displayResults(currentPageItems); 
-      pageCount++;
+      const currentPageItems = data.items.slice((currentPage - 1) * 20, currentPage * 20);
+      displayResults(currentPageItems);
     } else {
       $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
     }
   }
-  
 
   $(".search-btn").on("click", function() {
     const searchWord = $("#search-input").val().trim();
     if (searchWord !== searchLog) {
-      pageCount = 1;
+      currentPage = 1;
       searchLog = searchWord;
+      $(".lists").empty();
     }
 
     $.ajax({
@@ -50,14 +50,17 @@ $(function() {
       data: {
         title: searchWord,
         format: "json",
-        p: pageCount,
-        count: 20,
+        p: currentPage,
+        count: currentPage * 20,
       }
-    }).done(function(response) {
-      success(response);
-    }).fail(function(jqXHR) {
-      handleSearchFailure(jqXHR);
     })
+      .done(function(response) {
+        success(response);
+        currentPage++;
+      })
+      .fail(function(jqXHR) {
+        handleSearchFailure(jqXHR);
+      });
   });
 
   $(".reset-btn").on("click", function() {
@@ -65,6 +68,6 @@ $(function() {
     $(".message").remove();
     $("#search-input").val("");
     searchLog = "";
-    pageCount = 1;
+    currentPage = 1;
   });
 });
